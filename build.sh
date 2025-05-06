a23685e70a5b70f4081a8376d7e002e82129b2b7
Santiago GZ <sgallegoz02@gmail.com>
2025-05-06 19:32:59 -0400

    Descripción de los cambios realizados

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 # Aplica migraciones a la base de datos
-python manage.py migrate
+#python manage.py migrate
--- miapp/models.py
+++ miapp/models.py
@@ -27,7 +27,7 @@ class Categoria(models.Model):
         return self.categoria
 
 class Juego(models.Model):
-    nombre = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100, unique=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, blank=True)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     descripcion = models.TextField()
