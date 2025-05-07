a6501d2cfa1a89f6da45d760329486f29a18dfd5
Santiago GZ <sgallegoz02@gmail.com>
2025-05-06 20:06:10 -0400

    Actualiza build.sh y render.yaml para Render

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-# build.sh
 
-# Crea un entorno virtual y activa
-python -m venv venv
-source venv/bin/activate  # En Windows sería venv\Scripts\activate
+# Salir si ocurre un error
+set -o errexit
 
-# Instala los paquetes necesarios
+# Instala las dependencias
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# Recolecta los archivos estáticos (CSS, JS, imágenes)
-python manage.py collectstatic --noinput
-
-# Aplica migraciones a la base de datos
+# Aplica migraciones (si usas una base de datos)
 #python manage.py migrate
+
+# Recolecta archivos estáticos
+python manage.py collectstatic --no-input --clear
