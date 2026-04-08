b2ea6c31987e946d30f2e6fe0364b044b145b5b4
Santiago GZ <sgallegoz02@gmail.com>
2026-04-08 10:17:42 -0400

    fix render settings

--- levelup/settings.py
+++ levelup/settings.py
@@ -105,8 +105,8 @@ WSGI_APPLICATION = 'levelup.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
     )
